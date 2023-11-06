efcd441f6f88518b9298072f73bc8b3f05e9e840
Roman Manuel <romantmanuel@icloud.com>
2023-11-05 22:54:49 -0500

    Reread rubric and fixed mistakes

--- lab_9_challenge_3.py
+++ lab_9_challenge_3.py
@@ -3,25 +3,29 @@
 # COP 2500C Section 5
 # Nov 5, 2023
 
-# This function counts in each class how many times we get a better grade than the last grade
+# Original function from Challenge 1
 def grade_up(grade_list):
+    count = 0
+    for i in range(len(grade_list) - 1):
+        if (grade_list[i] < grade_list[i + 1]):
+            count += 1
+    return count
+
+
+# This function counts in each class how many times we improve our grade
+def counter(class_list):
 
     # This creates an empty list that we can store the results in
     results = []
 
-    # Loops through each row of classes
-    for row in range(len(grade_list)):
+    # Loops through the grades in each class
+    for grades in range(len(class_list)):
 
-        # Loops through each column of grades
-        counter = 0
-        for column in range(len(grade_list[row]) - 1):
-
-            # If current grade is better than the next grade, adds 1 to the counter
-            if (grade_list[row][column] < grade_list[row][column + 1]):
-                counter += 1
+        # Calls the grade_up function from Challenge 1
+        count = grade_up(class_list[grades])
 
         # Stores the count into the results list
-        results.append(counter)
+        results.append(count)
 
     # Returns the results back to main()
     return results
@@ -31,18 +35,13 @@ def grade_up(grade_list):
 def main():
 
     # A list with the grades for each class. [I got this from the assignment example]
-    Semester = [[95, 92, 93, 96, 92],
-                [100, 100],
-                [70, 80, 90],
-                [95, 85, 75, 70]]
+    Semester = [ [95, 92, 93, 96, 92], [100, 100], [70, 80, 90], [95, 85, 75, 70] ]
 
     # Calls the function called "grade_up" and gives it a list called "Semester"
-    results = grade_up(Semester)
+    results = counter(Semester)
 
     # Displays the results
     print(results)
-
-
 
 
 # This starts our program
